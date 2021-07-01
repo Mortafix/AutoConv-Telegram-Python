c7b2f62340ac315c9557fa1b18d6987357a3fd7f
Mortafix <moris.doratiotto@gmail.com>
2021-07-01 09:14:48 +0200

    Fix: in get state useless check with start state

--- autoconv/conversation.py
+++ autoconv/conversation.py
@@ -85,8 +85,6 @@ class Conversation:
     @validate_arguments
     def get_state(self, state_name: Optional[str]):
         """Get state from states list by name"""
-        if state_name == self.start.name:
-            return self.start
         for state in self.state_list:
             if state.name == state_name:
                 return state
